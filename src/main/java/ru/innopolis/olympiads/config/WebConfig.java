8933632fc1e03703edcb8289e4f53268eb1974f4
ilsaf13 <ilsaf13@yahoo.com>
2015-03-31 11:03:05 +0300

    undo admin fix

--- src/main/java/ru/innopolis/olympiads/config/WebConfig.java
+++ src/main/java/ru/innopolis/olympiads/config/WebConfig.java
@@ -54,7 +54,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("resources/");
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 //
 //    @Bean(name = "multipartResolver")
